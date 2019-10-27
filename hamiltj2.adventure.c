8f5623e0c74f16c2ca266680c016e5907fb5e08f
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-10-27 17:37:15 -0600

    Create required files

---
+++ hamiltj2.adventure.c
@@ -0,0 +1,10 @@
+//
+// Created by Jordan Hamilton on 10/27/19.
+//
+
+#include <stdio.h>
+
+int main() {
+  printf("Adventure\n");
+  return 0;
+}
---
+++ hamiltj2.buildrooms.c
@@ -0,0 +1,10 @@
+//
+// Created by Jordan Hamilton on 10/27/19.
+//
+
+#include <stdio.h>
+
+int main() {
+  printf("Buildrooms\n");
+  return 0;
+}
