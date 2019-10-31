6689b434168487b6ea7bfcee371d630b9c459315
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-10-30 21:29:58 -0600

    Initial versions

--- hamiltj2.adventure.c
+++ hamiltj2.adventure.c
@@ -1,10 +1,12 @@
-//
+/*
 // Created by Jordan Hamilton on 10/27/19.
-//
+*/
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
-int main() {
+int main(int argc, char* argv[]) {
   printf("Adventure\n");
   return 0;
 }
--- hamiltj2.buildrooms.c
+++ hamiltj2.buildrooms.c
@@ -1,10 +1,43 @@
-//
+/*
 // Created by Jordan Hamilton on 10/27/19.
-//
+*/
 
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+
+int getProcess() {
+  int pid = getpid();
+  return pid;
+}
+
+void makeDir(pid) {
+  char* dirName = malloc(21 * sizeof(char));
+  if (dirName == NULL)
+    printf("An error occurred.\n");
+
+  memset(dirName, '\0', 21);
+  sprintf(dirName, "hamiltj2.rooms.%d", getProcess());
+  mkdir(dirName, S_IRWXU);
+
+  free(dirName);
+}
+
+
 
 int main() {
+  char* thing1;
+  printf("%d\n", getProcess());
+  makeDir();
+
+
   printf("Buildrooms\n");
+
+  thing1 = malloc(10 * sizeof(char));
+  strcpy(thing1, "Testing");
+  printf(thing1);
+  free(thing1);
   return 0;
 }
