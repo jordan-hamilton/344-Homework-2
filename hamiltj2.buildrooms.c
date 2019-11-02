e8f5e8bc04a6d3b8c8cd431ebc89cac60273b3d4
Jordan Hamilton <jordan-hamilton@users.noreply.github.com>
2019-11-01 19:48:04 -0600

    Working connections

--- hamiltj2.buildrooms.c
+++ hamiltj2.buildrooms.c
@@ -1,8 +1,12 @@
 /*
 // Created by Jordan Hamilton on 10/27/19.
 */
+
+#define MIN_CONNECTIONS 3
+#define MAX_CONNECTIONS 6
+#define ROOMS_IN_GAME 7
 #define TOTAL_ROOMS 10
-#define ROOMS_TO_SELECT 7
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,24 +19,74 @@ struct room {
   int id;
   char* name;
   int numConnections;
+  struct room* connections[MAX_CONNECTIONS];
 };
 
+void addRoomConnection(struct room*);
+int alreadyConnected(struct room*, struct room*);
+void connectRooms(struct room*, struct room*);
 int getProcess();
-int getRand(int min, int max);
+int getRandomInt(int min, int max);
+struct room* getRandomRoom(struct room*);
+int hasMaxOutboundConnections(struct room*);
 void initRooms(struct room*);
 void makeDir();
 void nameRooms(struct room*);
 void makeFiles(struct room*);
+int roomConnectionsDone(struct room*);
+int sameRoom(struct room*, struct room*);
 
 int main() {
-  struct room gameRooms[ROOMS_TO_SELECT];
+  struct room gameRooms[ROOMS_IN_GAME];
   srand(time(0));
 
   makeDir();
   initRooms(gameRooms);
   makeFiles(gameRooms);
+  
+  int i;
+  int j;
+  for (i = 0; i < ROOMS_IN_GAME; i++) {
+    printf("----------------------\n\nRoom: %s\n", gameRooms[i].name);
+    for (j = 0; j < gameRooms[i].numConnections; j++) {
+      printf("Connection %d: %s\n", j+1, gameRooms[i].connections[j]->name);
+    }
+  }
 
   return 0;
+}
+
+void addRoomConnection(struct room* rooms) {
+  struct room *roomOne, *roomTwo;
+
+  do {
+    roomOne = getRandomRoom(rooms);
+  } while (hasMaxOutboundConnections(roomOne));
+
+  do {
+    roomTwo = getRandomRoom(rooms);
+  } while (sameRoom(roomOne, roomTwo) || hasMaxOutboundConnections(roomTwo) || alreadyConnected(roomOne, roomTwo));
+
+  connectRooms(roomOne, roomTwo);
+  printf("Room one, %s, is now connected to room two, %s.\n", roomOne->name, roomOne->connections[roomOne->numConnections - 1]->name);
+  printf("Room two, %s, is now connected to room one, %s.\n\n", roomTwo->name, roomTwo->connections[roomTwo->numConnections - 1]->name);
+}
+
+int alreadyConnected(struct room* roomOne, struct room* roomTwo) {
+  int i;
+  for (i = 0; i < roomOne->numConnections; i++) {
+    if (roomOne->connections[i]->id == roomTwo->id)
+      return 1;
+  }
+  return 0;
+}
+
+void connectRooms(struct room* roomOne, struct room* roomTwo) {
+  roomOne->connections[roomOne->numConnections] = roomTwo;
+  roomOne->numConnections++;
+  roomTwo->connections[roomTwo->numConnections] = roomOne;
+  roomTwo->numConnections++;
+  printf("\nConnected %s to %s\n", roomOne->name, roomTwo->name);
 }
 
 int getProcess() {
@@ -40,20 +94,36 @@ int getProcess() {
   return pid;
 }
 
-int getRand(int min, int max) {
+int getRandomInt(int min, int max) {
   /* Get a random number that's at most the difference between min and max, then add that to the min to get a number
    * in the provided range */
-  int randomNum = (rand() % (1 + max - min)) + min;
-  return randomNum;
+  int randomInt = (rand() % (1 + max - min)) + min;
+  return randomInt;
+}
+
+struct room* getRandomRoom(struct room* rooms) {
+  return &rooms[getRandomInt(0, ROOMS_IN_GAME - 1)];
+}
+
+int hasMaxOutboundConnections(struct room* room) {
+  if (room->numConnections == 6)
+    return 1;
+  else
+    return 0;
 }
 
 void initRooms(struct room* rooms) {
   int i;
-  for (i = 0; i < ROOMS_TO_SELECT; i++) {
+  for (i = 0; i < ROOMS_IN_GAME; i++) {
     rooms[i].id = i;
     rooms[i].numConnections = 0;
   }
   nameRooms(rooms);
+
+  while(!roomConnectionsDone(rooms)) {
+    printf("Adding a room connection.\n");
+    addRoomConnection(rooms);
+  }
 }
 
 void makeDir() {
@@ -70,7 +140,7 @@ void makeDir() {
 
 void makeFiles(struct room* gameRooms) {
   int i;
-  for (i = 0; i < ROOMS_TO_SELECT; i++)
+  for (i = 0; i < ROOMS_IN_GAME; i++)
     printf("Index: %d | Room name: %s\n", gameRooms[i].id, gameRooms[i].name);
 }
 
@@ -79,7 +149,7 @@ void nameRooms(struct room* rooms) {
   char* roomNames[TOTAL_ROOMS];
 
   roomNames[0] = "Kitchen";
-  roomNames[1] = "Hallway";
+  roomNames[1] = "Nursery";
   roomNames[2] = "Bathroom";
   roomNames[3] = "Den";
   roomNames[4] = "Closet";
@@ -89,8 +159,8 @@ void nameRooms(struct room* rooms) {
   roomNames[8] = "Bedroom";
   roomNames[9] = "Foyer";
 
-  while (roomsAdded < ROOMS_TO_SELECT) {
-    int roomToAdd = getRand(0, TOTAL_ROOMS - 1);
+  while (roomsAdded < ROOMS_IN_GAME) {
+    int roomToAdd = getRandomInt(0, TOTAL_ROOMS - 1);
 
     if (roomNames[roomToAdd] != NULL) {
       rooms[roomsAdded].name = roomNames[roomToAdd];
@@ -99,4 +169,20 @@ void nameRooms(struct room* rooms) {
       roomsAdded++;
     }
   }
+}
+
+int roomConnectionsDone(struct room* rooms) {
+  int i;
+  for (i = 0; i < ROOMS_IN_GAME; i++) {
+    if (rooms[i].numConnections < MIN_CONNECTIONS || rooms[i].numConnections > MAX_CONNECTIONS)
+      return 0;
+  }
+  return 1;
+}
+
+int sameRoom(struct room* roomOne, struct room* roomTwo) {
+  if (roomOne->id == roomTwo->id)
+    return 1;
+  else
+    return 0;
 }
